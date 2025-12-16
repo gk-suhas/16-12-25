bc4feea6f57d5f95d10a511ef9f5258c8f3e8c01
gk-suhas <karanamsuhas80@gmail.com>
2025-12-16 10:08:21 +0530

    Update comments for promise handling methods

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let p3 = Promise.reject("r3").catch(()=>{})
 let p4 = Promise.reject("r4").catch(()=>{})
 let p5 = Promise.resolve("r5")
 
-Promise.all([p1, p2, p5])               //reject if any 1 also fails
-       .then(res => console.log(res))
+Promise.all([p1, p2, p5])               //Resolves when all promises resolve
+       .then(res => console.log(res))   //Rejects if any promise fails
        .catch((err)=>console.log(err))
 
-Promise.allSettled([p1,p2,p3,p4,p5])    //waits for all promise sucess or fail
+Promise.allSettled([p1,p2,p3,p4,p5])    //Waits for all promises (success or failure)
        .then((res)=>console.log(res))
        .catch((err)=>console.log(err))
 
-Promise.race([p1,p2,p3,p4,p5])          //returns 1st promise
+Promise.race([p1,p2,p3,p4,p5])          //Returns result of the first promise to settle
        .then((res)=>console.log(res))
        .catch((err)=>console.log(err))
 
-Promise.any([p1,p2,p3,p4,p5])          // return any one ,rejects if all fail
-       .then((res)=>console.log(res))
+Promise.any([p1,p2,p3,p4,p5])          // Resolves when any one promise resolves
+       .then((res)=>console.log(res))  //Rejects only if all fail
        .catch((err)=>console.log(err))
